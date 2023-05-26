678147480e47246fd322ca04d4672adf0a7f5197
TrnHuyy <trnhuyworkspace@gmail.com>
2023-05-26 17:24:47 +0700

    Task HelloWorld 01 & 02

---
+++ HelloWorld/HelloWorld02/Entities/Student.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld02
+{
+    public class Student
+    {
+        public int Id {set; get;}
+        public string Name {set; get;}
+        public double Point {set; get;}
+    }
+}
