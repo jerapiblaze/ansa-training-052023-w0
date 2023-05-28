2b0ae73125c06f7248b3f9c1959947fbbb6140f7
xwomen1 <dohoikute@gmail.com>
2023-05-28 16:19:10 +0700

    helu

---
+++ HelloWorld/Thuylinh/bin/Hello.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Hello World");
+            
+        }
+    }
+}
