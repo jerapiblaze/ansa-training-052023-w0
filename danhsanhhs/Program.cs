0a8e6976b1b29bb37480be78918f808b596262b7
xwomen1 <dohoikute@gmail.com>
2023-05-29 10:16:27 +0700

    bai 2

---
+++ danhsanhhs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThucHanh2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap vao so luong sinh vien: ");
+        int n= Convert.ToInt32(Console.ReadLine());
+
+        SinhVien[] sinhVien = new SinhVien[n];
+
+        for(int i=0;i<n;i++)
+        {
+            sinhVien[i]= new SinhVien();
+            sinhVien[i].nhapSinhVien();
+        }
+        for(int i=0;i<n;i++)
+        {
+            sinhVien[i].hienthisinhvien();
+        }
+
+        }
+        
+    }
+}
