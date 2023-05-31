6142e7f635ea41953ebf78cee242104bce101aba
unknown <nguyenductien11122003@gmail.com>
2023-05-31 18:14:38 +0700

    NguyenTien_HelloWorld

---
+++ HelloWorld/NguyenTien/Task002/Program.cs
@@ -0,0 +1,21 @@
+﻿using Task002.Types;
+
+namespace Task002
+{
+    public class Task002
+    {
+        public static void Main(string[] args)
+        {
+            var listStudent = new List<Student>();
+            for (int i = 0;i < 10;i++) 
+            {
+                Student student = new(i , @$"h{9-i}el{i}lo", i * i / 10);
+                listStudent.Add(student);  
+            }
+            foreach (Student student in listStudent)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+}
