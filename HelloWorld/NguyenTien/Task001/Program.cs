02aee7c4be2cc15155b1c1ea870722867d0b5432
unknown <nguyenductien11122003@gmail.com>
2023-05-31 18:02:27 +0700

    Task001 done

---
+++ HelloWorld/NguyenTien/Task001/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Task001
+{
+    public class Program
+    {
+        static void Main(string[] args) 
+        {
+        Console.WriteLine("Hello World");
+        Console.ReadLine();
+        }
+    }
+}
