d51a63a498c560c66a7e2f95d16c05311eabb394
Jerapiblaze <73020288+jerapiblaze@users.noreply.github.com>
2023-06-02 15:44:16 +0700

    Revert "Do linh"

--- Reboot/ThuyLinh/Task02x/Models/TodoDb.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-namespace ThuyLinh.Models;
-public class TodoDb : DbContext
-{
-    public TodoDb(DbContextOptions<TodoDb> options)
-        : base(options) { }
-
-    public DbSet<Todo> Todos => Set<Todo>();
-}
