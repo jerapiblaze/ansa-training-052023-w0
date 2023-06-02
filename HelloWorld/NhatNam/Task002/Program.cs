7d4a57a478dcd9f8a2803536c399220ae41db527
Nhật Nam <nhatnam@unicornoffice.net>
2023-06-02 17:45:34 +0700

    “working-task002”

--- HelloWorld/NhatNam/Task002/Program.cs
+++ HelloWorld/NhatNam/Task002/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Program
+{
+    public class Student
+    {
+        public string? Name;
+
+        public int ID;
+
+        public string? Class;
+
+        public static int NumberOfStudents = 0;
+
+        // Constructor
+        public Student(string Name, int ID, string Class)
+        {
+            this.Name = Name;
+            this.ID = ID;
+            this.Class = Class;
+            NumberOfStudents++;
+        }
+
+        public void printStudent()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("ID: " + ID);
+            Console.WriteLine("Class: " + Class);
+        }
+
+
+    }
+}
