0c075a99622fbdc9db41cc02e91adfbf5127787d
qminh1908 <qminh1908@gmail.com>
2023-06-04 23:33:42 +0700

    Task01 Fixed

--- Reboot/QuangMinh/0.2.1/Student.cs
+++ Reboot/QuangMinh/0.2.1/Student.cs
@@ -1,12 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace _0._2._1
 {
     public class Student
     {
-        
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public override string ToString() => JsonSerializer.Serialize<Student>(this);
     }
 }
